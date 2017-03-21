423c60319359b075597d8d110ccf2029df9f1e54
Dantist <dantist11@bk.ru>
2017-03-22 00:33:44 +0300

    Добавил программу "Поиск анаграмм по словарю".

--- homeworks/anagrams/lib/Anagram.pm
+++ homeworks/anagrams/lib/Anagram.pm
@@ -1,8 +1,10 @@
 package Anagram;
 
-use 5.010;
 use strict;
 use warnings;
+use 5.020;
+use utf8;
+use Encode;
 
 =encoding UTF8
 
@@ -40,14 +42,76 @@ anagram(['пятак', 'ЛиСток', 'пятка', 'стул', 'ПяТаК', '
 =cut
 
 sub anagram {
-    my $words_list = shift;
-    my %result;
+	my $words_list = shift;
+	my %result = ();
+	for (@$words_list){
+		Encode::_utf8_on($_);
+	}
+	@$words_list 	=  map  {lc $_} @$words_list;
+	for (@$words_list){
+		if (defined $_){
+			my $key = $_;
+			$_=undef;
+	
+			for (@$words_list){
+				
+				if (is_equal($_, $key) and not contain_equals($_,$result{$key})){
+					
+					$result{$key} = $result{$key}||[$key];
+					$result{$key} = [@{$result{$key}}, $_];
+	
+					$_ = undef;
+	
+				}
+				if (contain_equals($_,$result{$key})){
+					$_ = undef;
+				}
+	
+			}
+	
+		}
+	}	
 
-    #
-    # Поиск анограмм
-    #
+	my %result2;
+	for (values %result){
+		@$_ =  sort { fc($a) cmp fc($b) }@$_;
+		@$_ = map {encode ('utf8', $_)}@$_;
+	}
+	for (keys %result){
+		$result2{encode ('utf8', $_)} = $result{$_};
+	}
+	return \%result2;
+}
+sub is_equal{
+	my $one = shift;
+	my $two = shift;
+	if ( not defined $one or not defined $two){
+		return 0;
+	}
 
-    return \%result;
+	if (length $one == length $two){
+		for (split ('', $two)){
+			last unless($one =~ s/$_//i)
+	}
+	if (length $one == 0){
+		return 1;
+	}else {
+		return 0;
+	}
+
+		
+	}else {
+		return 0;
+	}
+
+}
+
+sub contain_equals{
+	my $scalar = shift;
+	my $list = shift;
+	if (defined $list and defined $scalar and  scalar (grep{fc($scalar) eq fc($_)}@{$list}) >0){
+		return 1;
+	}else {return 0};
 }
 
 1;
